8a1c2ae2cc12938d4b2323565129d8c564c2762d
Volkov Denis <volkov.denis.dev@yandex.ru>
2019-10-17 16:10:16 +0500

    add homework 3

---
+++ Homework3/LetterBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace Homework3
+{
+    public class LetterBuilder
+    {
+        private readonly string _from;
+        private readonly string _to;
+        private readonly string _body;
+
+        public LetterBuilder(string to, string body)
+        {
+            _from = "admin@admin.ru"; // здесь должно быть получение реального адреса
+            _to = to;
+            _body = body;
+        }
+
+        public OptionalLetterBuilder AddSubject(string subject)
+        {
+            return new OptionalLetterBuilder(_from, _to, _body).AddSubject(subject);
+        }
+
+        public OptionalLetterBuilder AddCopyRecipient(string recipient)
+        {
+            return new OptionalLetterBuilder(_from, _to, _body).AddCopyRecipient(recipient);
+        }
+
+        public OptionalLetterBuilder AddCopyRecipients(IEnumerable<string> recipients)
+        {
+            return new OptionalLetterBuilder(_from, _to, _body).AddCopyRecipients(recipients);
+        }
+
+        public class OptionalLetterBuilder
+        {
+            private Letter _letter;
+
+            public OptionalLetterBuilder(string from, string to, string body)
+            {
+                _letter = new Letter
+                {
+                    From = from,
+                    To = new List<string> { to },
+                    Body = body
+                };
+            }
+
+            public OptionalLetterBuilder AddSubject(string subject)
+            {
+                _letter.Subject = subject;
+                return this;
+            }
+
+            public OptionalLetterBuilder AddCopyRecipient(string recipient)
+            {
+                _letter.To.Add(recipient);
+                return this;
+            }
+
+            public OptionalLetterBuilder AddCopyRecipients(IEnumerable<string> recipients)
+            {
+                _letter.To.AddRange(recipients);
+                return this;
+            }
+
+            public string Build()
+            {
+                var letter = new[]
+                {
+                    $"From: {_letter.From}",
+                    $"To: {string.Join(", ", _letter.To)}",
+                    $"Subject: {_letter.Subject ?? "*empty*"}",
+                    string.Empty,
+                    $"{_letter.Body}",
+                    "\n\r"
+                };
+                return string.Join("\n\r", letter);
+            }
+        }
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letter1 = new LetterBuilder("admin@admin.ru", "This is very useful message!")
+                .AddSubject("Spam :)")
+                .AddCopyRecipient("notadmin@admin.ru")
+                .AddCopyRecipients(new[] {"thisisdefenetelynot@admin.ru", "ad@min.ru"})
+                .Build();
+            Console.WriteLine(letter1);
+
+            var letter2 = new LetterBuilder("death@no.te", "You shouldn't have read it. Now you are going to die!")
+                .AddCopyRecipient("all@world.org")
+                .Build();
+            Console.WriteLine(letter2);
+        }
+    }
+}
